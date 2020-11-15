eb1984eb2408859cf40e65717162158bb558c9f3
erin dougher <ejdougher@gmail.com>
2020-11-15 05:54:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ introduction("Erin")
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-introduction_with_name("Erin", "Ruby")
+introduction_with_language("Erin", "Ruby")
 
 def introduction_with_optional(name, language = 'Ruby')
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
